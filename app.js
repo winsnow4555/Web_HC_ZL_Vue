1b17d4d2c6fa560c8c1ab9b03772b1de19a539a8
winsnow4555 <winsnow4555@gmail.com>
2020-03-29 12:22:41 +0800

    連結 非同步 AJAX Axios CDN

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ var content = new Vue({
     el: "#content",
     data: {
         name: ""
+    },
+    methods: {
+        // 取得資料庫
+        getRepos: function (){
+            
+        }
     }
 });
