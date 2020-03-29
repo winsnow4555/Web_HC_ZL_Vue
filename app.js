c5bb6c2142adfc798c4f6d0ce83137969f03c4e9
winsnow4555 <winsnow4555@gmail.com>
2020-03-29 12:52:31 +0800

    完成

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var content = new Vue({
     el: "#content",
     data: {
-        name: ""
+        name: "",   // "" 空的文字
+        repos: null // null 空值
     },
     methods: {
         // 取得資料庫
-        getRepos: function (){
-            
+        getRepos: function () {
+            console.log("取得資料庫" + this.name);
+            // axios 取得資了 (資料連結 API).取得完成後 (回應 => { 回應資料存取 })
+            axios
+            .get(`https://api.github.com/users/${this.name}/repos`)
+            .then(response => {this.repos = response.data; console.log(response.data)})
         }
     }
 });
